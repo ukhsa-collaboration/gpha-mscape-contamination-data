c3bea0709f943826e0e4a648a438676e1ba91061
Rachel Colquhoun <rachel.colquhoun@ed.ac.uk>
2025-03-18 14:56:38 +0000

    remove kraken report name requirement

--- bin/make_sum_report.py
+++ bin/make_sum_report.py
@@ -185,7 +185,7 @@ def get_species_count(needed_samples, reports, microbe_type, taxon_level, filter
 #   Loop over each kraken file in reports directory
     for sample in needed_samples:
         for filename in reports:
-            if f'{sample}' in filename and filename.endswith('.kraken2.report.txt'):
+            if f'{sample}' in filename:
                 #open new file and read it line by line
                 file = open(filename)
         
@@ -369,7 +369,7 @@ def make_perc_df(needed_samples, reports):
 #   Loop over each kraken file in reports directory
     for sample in needed_samples:
         for filename in reports:
-            if f'{sample}' in filename and filename.endswith('.kraken2.report.txt'):
+            if f'{sample}' in filename:
                 #open new file and read it line by line
                 file = open(filename)
         
